b469ee51ac6883664171f68b8262d0036fda3418
E-Albert <magicspoon123@gmail.com>
2022-01-07 20:46:08 -0500

    initial commit

---
+++ Develop/script.js
@@ -0,0 +1,7 @@
+// I want to to have a daily date on the page
+// I want the colors to change with the time
+// I want to save to local Storage
+// alert message: "I just saved to local storage"
+
+$("#currentDay").text(moment().format("MMM Do YY"));
+
