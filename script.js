3f7836082b0267dda53a63153d9f6d956a4aecbb
E-Albert <magicspoon123@gmail.com>
2022-02-06 00:11:48 -0500

    Added local storage

--- script.js
+++ script.js
@@ -1,36 +1,20 @@
-
-// I want to save to local Storage
-// alert message: "I just saved to local storage"
-
 //Putting the date in the jumbotron
 $("#currentDay").text(moment().format("MMM Do YY"));
 
-
-//grabbing the DOM elements that are going to be interacted with by the user
-let nineButton = document.querySelector('.nineAmButton');
-let tenButton = document.querySelector('.tenAmButton');
-let elevenButton = document.querySelector('.elevenAmButton');
-let twelveButton = document.querySelector('.twelvePmButton');
-let oneButton = document.querySelector('.onePmButton');
-let twoButton = document.querySelector('.twoPmButton');
-let threeButton = document.querySelector('.threePmButton');
-let fourButton = document.querySelector('.fourPmButton');
-let fiveButton = document.querySelector('.fivePmButton');
-
 //DOM element that is going to be manipulated and saved to local storage
-let colorBar = document.getElementsByTagName('textarea');
+let colorBar = $('textarea');
 timeTeller();
 
 function timeTeller() {
     //grabbing the current hour and saving it into a variable
     let currentHour = moment().hour();
-    console.log(currentHour);
-    //writing a function for each text area to grab its id and convert 
-    //it to a number so that I can compare it to the current hour
+    //console.log(currentHour);
+    /*writing a function for each text area to grab its id and convert 
+    it to a number so that I can compare it to the current hour*/
     $(colorBar).each(function () {
         //grabbing the id of each text area and parsing it to compare to current hour
         let workDayHour = parseInt($(this).attr("id"));
-        console.log("Work day hour: " + workDayHour);
+        //console.log("Work day hour: " + workDayHour);
         //conditions for making the background of each textarea specific colors
         if (currentHour > workDayHour) {
             $(this).addClass("past");
@@ -43,5 +27,29 @@ function timeTeller() {
             //console.log("This time is currently in the present");
         }
     })
-}    
-   
+}
+
+//setting up the local storage
+$(".saveBtn").on('click', function () {
+    //console.log(this);
+    //grabs the text from sibling element with the class of description
+    var text = $(this).siblings(".description").val();
+    //console.log(text)
+    /*grabs the id of parent element so that it can be the name of the text area value when saved to local storage*/
+    var hourText = $(this).parent().attr("id");
+    //console.log(hourText)
+    //set items in local storage.
+    localStorage.setItem(hourText, text);
+})
+
+//grabbing descriptions that were placed in local storage for each row
+$("#nineAmRow .description").val(localStorage.getItem("nineAmRow"));
+$("#tenAmRow .description").val(localStorage.getItem("tenAmRow"));
+$("#elevenAmRow .description").val(localStorage.getItem("elevenAmRow"));
+$("#twelvePmRow .description").val(localStorage.getItem("twelvePmRow"));
+$("#onePmRow .description").val(localStorage.getItem("onePmRow"));
+$("#twoPmRow .description").val(localStorage.getItem("twoPmRow"));
+$("#threePmRow .description").val(localStorage.getItem("threePmRow"));
+$("#fourPmRow .description").val(localStorage.getItem("fourPmRow"));
+$("#fivePmRow .description").val(localStorage.getItem("fivePmRow"));
+
